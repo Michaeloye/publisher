8abd728219887925055750220b8ed27425d63f5d
michaeloye <michaeloye23@gmail.com>
2022-03-21 16:57:53 +0100

    Add Logo, SearchBar, Notification, ProfilePicture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Logo from "./Logo";
+import Notification from "./Notification";
+import ProfilePicture from "./ProfilePicture";
+import SearchBar from "./SearchBar";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="h-12 md:h-14 bg-white border flex items-center justify-between">
+      <div className="ml-3 md:ml-10">
+        <Logo />
+      </div>
+      <SearchBar />
+
+      {/* Notification and ProfilePicture are in a single div because from the design they are closer
+      to each other  */}
+      <div className="flex items-center gap-5">
+        <Notification />
+        <div className="mr-3 md:mr-10">
+          <ProfilePicture />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
