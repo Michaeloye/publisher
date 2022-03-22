559b02ee321248bd90b0ca41762d45090f598440
michaeloye <michaeloye23@gmail.com>
2022-03-22 12:13:48 +0100

    Adjust gap

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -3,7 +3,7 @@ import NavElement from "../NavElement";
 
 function SideNav() {
   return (
-    <div className="md:w-[10%] lg:w-[32%] md:flex md:flex-col lg:items-start md:gap-5 md:items-center md:mt-10">
+    <div className="md:w-[10%] lg:w-[32%] md:flex md:flex-col lg:items-start md:gap-7 lg:gap-5 md:items-center md:mt-10">
       <NavElement iconNumber={1} text="Home" active={true} />
       <NavElement iconNumber={2} text="Explore" />
       <NavElement iconNumber={3} text="Topics" />
