6846683f6e130587f9c365e5e558521aed3d6a5d
michaeloye <michaeloye23@gmail.com>
2022-03-22 23:23:06 +0100

    Show like or dislike color onClick

--- src/components/LikeDislike.jsx
+++ src/components/LikeDislike.jsx
@@ -5,20 +5,28 @@ function LikeDislike({ number }) {
   // the below state is used to toggle the like or dislike button
   const [like, setLike] = useState(true);
   const [dislike, setDislike] = useState(true);
+  const [vote, setVote] = useState(number);
   return (
-    <div className="flex flex-col items-center gap-1">
+    <div className="flex flex-col items-center gap-1 cursor-pointer">
       <FaArrowUp
         color={like ? "gray" : "#1a62dc"}
         size={20}
-        onClick={() =>
-          setLike((prevState) => setLike(!prevState), console.log(like))
-        }
+        onClick={() => {
+          setLike(false);
+          setDislike(true);
+          setVote(number + 1);
+        }}
       />
-      <p className="text-gray-500 text-sm">{number}</p>
+      {/* It is necessary that that the number color is the color of like or dislike */}
+      <p className="text-gray-500 text-sm">{vote}</p>
       <FaArrowDown
-        color="gray"
+        color={dislike ? "gray" : "red"}
         size={20}
-        onClick={() => console.log("hello")}
+        onClick={() => {
+          setDislike(false);
+          setLike(true);
+          setVote(number - 1);
+        }}
       />
     </div>
   );
