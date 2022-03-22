3d59f8d465d32b3c5fcb32bd0e872a414de9bab5
michaeloye <michaeloye23@gmail.com>
2022-03-22 12:00:04 +0100

    Add BottomNav.jsx

---
+++ src/components/BottomNav/BottomNav.jsx
@@ -0,0 +1,14 @@
+import NavElement from "../NavElement";
+
+function BottomNav() {
+  return (
+    <div className="flex bg-primary-gray rounded-tl-2xl rounded-tr-2xl w-[75%] h-12 justify-center items-center gap-10 z-10 absolute bottom-0 left-[25%] -ml-11">
+      <NavElement iconNumber={1} active={true} />
+      <NavElement iconNumber={2} />
+      <NavElement iconNumber={3} />
+      <NavElement iconNumber={4} />
+    </div>
+  );
+}
+
+export default BottomNav;
