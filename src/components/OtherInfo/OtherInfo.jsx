428b9b699e4b19776b2b3138bb9fb1aacfa47e68
michaeloye <michaeloye23@gmail.com>
2022-03-23 00:47:14 +0100

    Add OtherInfo.jsx

---
+++ src/components/OtherInfo/OtherInfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StartTopic from "../StartTopic";
+
+function OtherInfo() {
+  return (
+    <div className="bg-primary-gray md:w-1/3 lg:w-1/2 flex flex-col items-center">
+      <StartTopic />
+    </div>
+  );
+}
+
+export default OtherInfo;
