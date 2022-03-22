fa2981426061c5dababf48bb94ca25b072e99baa
michaeloye <michaeloye23@gmail.com>
2022-03-22 11:17:27 +0100

    Make scrollable and scrollbar invisible

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="bg-blue-900 w-full h-screen overflow-y-scroll" id="home">
+      {/* The above styling is what enalbes the center page to be scrollable. The scroll bar
+      is invisible thanks to the styling in index.css #home */}
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="bg-black">nad</div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
