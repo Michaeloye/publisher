b28d88ee2259e342f56cc4b25ccd9d311976dc2a
michaeloye <michaeloye23@gmail.com>
2022-03-22 23:03:09 +0100

    Add Comment component with props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Comment from "../components/Comment/Comment";
 
 function Home() {
   return (
-    <div className="bg-blue-900 w-full h-screen overflow-y-scroll" id="home">
+    <div
+      className="bg-primary-gray w-full h-screen overflow-y-scroll"
+      id="home"
+    >
       {/* The above styling is what enalbes the center page to be scrollable. The scroll bar
       is invisible thanks to the styling in index.css #home */}
       <div>
@@ -11,6 +15,20 @@ function Home() {
         <br />
         <br />
         <br />
+        <Comment
+          title="lorem ispium"
+          description="
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
+        "
+          author="andrew neil"
+        />
+        <Comment
+          title="lorem ispium"
+          description="
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
+      "
+          author="andrew neil"
+        />
         <br />
         <br />
         <br />
