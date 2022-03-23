6b2a86a3c44cc49013ae9a5c7807c94c9f8c438d
michaeloye <michaeloye23@gmail.com>
2022-03-23 02:35:14 +0100

    Add TopTopics, QuickPost components

--- src/components/OtherInfo/OtherInfo.jsx
+++ src/components/OtherInfo/OtherInfo.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import QuickPost from "../QuickPost/QuickPost";
 import StartTopic from "../StartTopic";
+import TopTopics from "../TopTopics/TopTopics";
 
 function OtherInfo() {
   return (
-    <div className="bg-primary-gray md:w-1/3 lg:w-1/2 flex flex-col items-center">
-      <StartTopic />
+    <div className="bg-primary-gray md:w-1/3 lg:w-1/2 flex flex-col items-center gap-10">
+      <div className="mt-5">
+        <StartTopic />
+      </div>
+      <TopTopics />
+      <QuickPost />
     </div>
   );
 }
