b33385b0a00c44051f019ba9ef1b0d528fa9997c
michaeloye <michaeloye23@gmail.com>
2022-03-23 16:29:53 +0100

    Add Footer component

--- src/components/OtherInfo/OtherInfo.jsx
+++ src/components/OtherInfo/OtherInfo.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 import QuickPost from "../QuickPost/QuickPost";
 import StartTopic from "../StartTopic";
 import TopTopics from "../TopTopics/TopTopics";
 
 function OtherInfo() {
   return (
-    <div className="bg-primary-gray md:w-1/3 lg:w-1/2 flex flex-col items-center gap-10">
+    <div
+      className="bg-primary-gray md:w-1/3 lg:w-1/2 flex flex-col items-center gap-10 h-screen overflow-y-scroll overflow-x-hidden border-2"
+      id="other-info"
+    >
       <div className="mt-5">
         <StartTopic />
       </div>
       <TopTopics />
       <QuickPost />
+      <Footer />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
