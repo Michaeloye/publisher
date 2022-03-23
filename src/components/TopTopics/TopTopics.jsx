109caa9479347c30fd8665b9e6c270f2b6a375b0
michaeloye <michaeloye23@gmail.com>
2022-03-23 02:35:26 +0100

    Add TopTopics.jsx

---
+++ src/components/TopTopics/TopTopics.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import UserTopic from "../UserTopic";
+import BookImage from "../../assets/book.jpg";
+
+function TopTopics() {
+  return (
+    <div className="bg-white md:w-3/4 rounded-2xl pb-5 shadow-lg">
+      <div
+        className="md:h-14 rounded-tr-3xl rounded-tl-3xl opacity-80 shadow-lg"
+        style={{
+          backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.72), rgba(0, 0, 0, 0.72)), url(${BookImage})`,
+        }}
+      >
+        <p className="text-white font-semibold inline-block mt-5">Top Topics</p>
+      </div>
+      <UserTopic />
+      <UserTopic />
+      <UserTopic />
+      <UserTopic />
+    </div>
+  );
+}
+
+export default TopTopics;
