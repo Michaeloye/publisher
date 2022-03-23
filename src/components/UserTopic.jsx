1d80994661dd0d0ff057a9b2544a668a81c5ebbf
michaeloye <michaeloye23@gmail.com>
2022-03-23 16:05:02 +0100

    Update style

--- src/components/UserTopic.jsx
+++ src/components/UserTopic.jsx
@@ -3,9 +3,9 @@ import ProfilePicture from "./ProfilePicture";
 
 function UserTopic() {
   return (
-    <div className="flex justify-center items-center gap-10 mt-2 text-gray-600">
+    <div className="flex justify-center items-center md:gap-2 lg:gap-10 mt-2 text-gray-600">
       <div className="flex justify-center items-center gap-2">
-        <ProfilePicture /> <p className="text-sm">UserName</p>
+        <ProfilePicture /> <p className="text-sm">Name</p>
       </div>
       <p className="text-sm">Topic</p>
     </div>
