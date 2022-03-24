76abddbaec34f6933855671aaed7ec2844e55cdb
michaeloye <michaeloye23@gmail.com>
2022-03-24 15:26:34 +0100

    Update padding

--- src/components/StartTopic.jsx
+++ src/components/StartTopic.jsx
@@ -3,9 +3,9 @@ import { BsPlus } from "react-icons/bs";
 
 function StartTopic() {
   return (
-    <div className="bg-primary-blue h-10 w-60 px-10 rounded-lg text-white flex items-center">
+    <div className="bg-primary-blue w-40 h-10 md:w-60 md:px-10 rounded-lg text-white flex items-center justify-center">
       <BsPlus size={25} />
-      <p className="font-semibold">Start New Topic</p>
+      <p className="font-semibold text-sm md:text-base">Start New Topic</p>
     </div>
   );
 }
