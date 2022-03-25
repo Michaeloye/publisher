587d4a2bccb0a7b807ae2efaca321813344a9453
michaeloye <michaeloye23@gmail.com>
2022-03-25 23:07:23 +0100

    Add white background

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Backdrop from '../Backdrop';
-import close from '../../assets/svgs/close.svg';
+import React from "react";
+import Backdrop from "../Backdrop";
+import close from "../../assets/close.svg";
 
-export default function Modal({ handleClose, image }) {
+export default function Modal({ handleClose }) {
   return (
     <Backdrop>
       <div
-        className="relative mx-auto h-full w-full flex items-center justify-center"
+        className="relative mx-auto h-96 w-96 flex bg-white items-center justify-center"
         onClick={(e) => e.stopPropagation()}
       >
         <img
           src={close}
           onClick={() => handleClose(false)}
           className="h-7 w-7 absolute cursor-pointer"
-          style={{ top: '5%', right: '10%' }}
+          style={{ top: "5%", right: "10%" }}
         />
-        <img src={image} alt="image-of-product" className="w-auto" style={{ maxHeight: '90vh' }} />
+        {/* <img src={image} alt="image-of-product" className="w-auto" style={{ maxHeight: '90vh' }} /> */}
       </div>
     </Backdrop>
   );
