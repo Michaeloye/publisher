97324edd405038bbbb6bd4d04c62a63fee27dbea
michaeloye <michaeloye23@gmail.com>
2022-03-27 17:29:54 +0100

    Add onChange prop

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,11 +3,11 @@ import { RiLockPasswordFill } from "react-icons/ri";
 import { GrMail } from "react-icons/gr";
 
 const Icons = {
-  1: <FaUser className="absolute top-3 text-gray-300 left-3 " />,
+  1: <FaUser className="absolute top-2 text-gray-300 left-3 " />,
   2: <GrMail className="absolute top-2 text-gray-300 left-3 " />,
   3: <RiLockPasswordFill className="absolute top-2 text-gray-300 left-3 " />,
 };
-function Input({ icon, type, name, placeHolder }) {
+function Input({ icon, type, name, placeHolder, changePassword }) {
   return (
     <div className="relative">
       <input
@@ -17,6 +17,7 @@ function Input({ icon, type, name, placeHolder }) {
         autoComplete="off"
         spellCheck="false"
         className="h-10 md:h-9 px-9 py-1 rounded-full bg-white text-sm focus:outline-none lg:text-base border shadow-sm text-gray-700"
+        onChange={changePassword}
       />
       {Icons[icon]}
     </div>
