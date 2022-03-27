1c0daaff7e826afc80619ba2c61ce43b4ed48784
michaeloye <michaeloye23@gmail.com>
2022-03-27 16:18:55 +0100

    Add Input.jsx

---
+++ src/components/Input.jsx
@@ -0,0 +1,26 @@
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { GrMail } from "react-icons/gr";
+
+const Icons = {
+  1: <FaUser className="absolute top-3 text-gray-300 left-3 " />,
+  2: <GrMail className="absolute top-2 text-gray-300 left-3 " />,
+  3: <RiLockPasswordFill className="absolute top-2 text-gray-300 left-3 " />,
+};
+function Input({ icon, type, name, placeHolder }) {
+  return (
+    <div className="relative">
+      <input
+        type={type}
+        name={name}
+        placeholder={placeHolder}
+        autoComplete="off"
+        spellCheck="false"
+        className="h-10 md:h-9 px-9 py-1 rounded-full bg-white text-sm focus:outline-none lg:text-base border shadow-sm text-gray-700"
+      />
+      {Icons[icon]}
+    </div>
+  );
+}
+
+export default Input;
