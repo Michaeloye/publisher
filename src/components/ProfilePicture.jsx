513c2332d952bd60e1ba4612a35b940b9b0fcf62
michaeloye <michaeloye23@gmail.com>
2022-03-29 22:15:34 +0100

    Add size prop; make style according to size prop

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function ProfilePicture() {
-  return <div className="bg-gray-300 h-9 w-9 rounded-full"></div>;
+function ProfilePicture({ size = "medium" }) {
+  return (
+    <div
+      className={
+        "bg-gray-300 rounded-full " +
+        (size == "small"
+          ? "h-5 w-5 "
+          : size == "medium"
+          ? "h-9 w-9 "
+          : size == "large"
+          ? "h-20 w-20 md:h-32 md:w-32 "
+          : "")
+      }
+    ></div>
+  );
 }
 
 export default ProfilePicture;
