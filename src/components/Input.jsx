7b33d333220cda299935d64aac6e35ad06af55a2
michaeloye <michaeloye23@gmail.com>
2022-03-30 15:22:02 +0100

    Rename function prop

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ const Icons = {
   2: <GrMail className="absolute top-2 text-gray-300 left-3 " />,
   3: <RiLockPasswordFill className="absolute top-2 text-gray-300 left-3 " />,
 };
-function Input({ icon, type, name, placeHolder, changePassword }) {
+function Input({ icon, type, name, placeHolder, changeInput }) {
   return (
     <div className="relative">
       <input
@@ -17,7 +17,7 @@ function Input({ icon, type, name, placeHolder, changePassword }) {
         autoComplete="off"
         spellCheck="false"
         className="h-10 md:h-9 px-9 py-1 rounded-full bg-white text-sm focus:outline-none lg:text-base border border-gray-600 shadow-sm text-gray-700"
-        onChange={changePassword}
+        onChange={changeInput}
       />
       {Icons[icon]}
     </div>
