549351441dcec809d4af00b70d7030e13a65cb2d
michaeloye <michaeloye23@gmail.com>
2022-04-11 01:42:53 +0100

    Add userName prop

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ProfilePicture from "./ProfilePicture";
 
-function ProfileDetails() {
+function ProfileDetails({ userName }) {
   return (
     <div className="flex items-center ml-10 md:ml-20 mt-10 gap-7">
       <ProfilePicture size="large" />
-      <h1 className="text-lg md:text-2xl font-bold">Andrew Davison</h1>
+      <h1 className="text-lg md:text-2xl font-bold">{userName}</h1>
     </div>
   );
 }
