13c6f307471f614a88335592225e0351c08893c5
michaeloye <michaeloye23@gmail.com>
2022-04-11 01:42:41 +0100

    Add Link

--- src/components/NavElement.jsx
+++ src/components/NavElement.jsx
@@ -1,6 +1,7 @@
 import { IoMdBulb } from "react-icons/io";
 import { IoHome } from "react-icons/io5";
 import { MdExplore, MdInsertComment } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const iconList = {
   1: <IoHome size={25} />,
@@ -8,9 +9,16 @@ const iconList = {
   3: <IoMdBulb size={25} />,
   4: <MdInsertComment size={25} />,
 };
+const navList = {
+  1: "/feed",
+  2: "/explore",
+  3: "/topics",
+  4: "/mycomments",
+};
 function NavElement({ iconNumber, text, active }) {
   return (
-    <div
+    <Link
+      to={navList[iconNumber]}
       className={
         "lg:flex lg:justify-center lg:gap-3 lg:ml-10 cursor-pointer " +
         (active ? "text-primary-blue" : "text-gray-400")
@@ -18,7 +26,7 @@ function NavElement({ iconNumber, text, active }) {
     >
       {iconList[iconNumber]}
       <p className="md:hidden lg:block lg:text-lg lg:font-semibold ">{text}</p>
-    </div>
+    </Link>
   );
 }
 
