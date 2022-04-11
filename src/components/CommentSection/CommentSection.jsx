555d08b6d9bd440efa1a848dcd324eebdb40ab3f
michaeloye <michaeloye23@gmail.com>
2022-04-11 19:59:06 +0100

    Get posts from api using axios

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,6 +1,16 @@
+import { useState, useEffect } from "react";
 import Comment from "../Comment/Comment";
+import axios from "axios";
 
 function CommentSection() {
+  const [posts, setPosts] = useState([]);
+  // be sure if should only fetch comments on mount
+  useEffect(() => {
+    axios
+      .get("https://mikepostapp.herokuapp.com/feed/posts")
+      .then((res) => setPosts(res.data.posts))
+      .catch((err) => console.log(err.message));
+  }, []);
   return (
     <div
       className="bg-primary-gray w-full h-screen overflow-y-scroll"
@@ -12,35 +22,37 @@ function CommentSection() {
         <br />
         <Comment
           title="lorem ispium"
-          description="
+          content="
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
         "
           author="andrew neil"
         />
+        <Comment title="lorem ispium" content="" author="andrew neil" />
+        {posts.map((post) => (
+          <Comment
+            key={post._id}
+            title={post.title}
+            content={post.content}
+            author={post.creator.name}
+          />
+        ))}
         <Comment
           title="lorem ispium"
-          description="
+          content="
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
       "
           author="andrew neil"
         />
         <Comment
           title="lorem ispium"
-          description="
+          content="
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
       "
           author="andrew neil"
         />
         <Comment
           title="lorem ispium"
-          description="
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-      "
-          author="andrew neil"
-        />
-        <Comment
-          title="lorem ispium"
-          description="
+          content="
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
       "
           author="andrew neil"
