024558eca6f9775c22f340b9f86be5c1438d9a0f
michaeloye <michaeloye23@gmail.com>
2022-04-12 13:02:53 +0100

    Remove default comment

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -17,17 +17,9 @@ function CommentSection() {
       id="comment-section"
     >
       {/* The above styling is what enalbes the center page to be scrollable. The scroll bar
-      is invisible thanks to the styling in index.css #comment-section */}
+      is invisible because of the styling in index.css #comment-section */}
       <div>
         <br />
-        <Comment
-          title="lorem ispium"
-          content="
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-        "
-          author="andrew neil"
-        />
-        <Comment title="lorem ispium" content="" author="andrew neil" />
         {posts.map((post) => {
           return (
             <Comment
@@ -39,27 +31,6 @@ function CommentSection() {
             />
           );
         })}
-        <Comment
-          title="lorem ispium"
-          content="
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-      "
-          author="andrew neil"
-        />
-        <Comment
-          title="lorem ispium"
-          content="
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-      "
-          author="andrew neil"
-        />
-        <Comment
-          title="lorem ispium"
-          content="
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-      "
-          author="andrew neil"
-        />
         <br />
         <br />
         <br />
