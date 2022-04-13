2e326dfbe9194f71f53d7b6467d5a0693c44caa9
michaeloye <michaeloye23@gmail.com>
2022-04-13 23:06:52 +0100

    Add onClick callback prop function

--- src/components/StartTopic.jsx
+++ src/components/StartTopic.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BsPlus } from "react-icons/bs";
 
-function StartTopic() {
+function StartTopic({ onClick }) {
   return (
     <button
       className="bg-primary-blue flex items-center justify-center w-12 h-12 md:w-60 md:px-10 rounded-full md:rounded-lg text-white 
      fixed bottom-12 right-5 md:static md:bottom-auto md:right-auto"
+      onClick={() => onClick()}
     >
       <BsPlus size={25} />
       <p className="hidden md:block font-semibold text-sm md:text-base">
