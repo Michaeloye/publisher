a265836f34b162da31562a282de0d587b4e1b74a
michaeloye <michaeloye23@gmail.com>
2022-04-13 18:02:34 +0100

    Change 'absolute' to 'fixed'

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -2,7 +2,7 @@ import NavElement from "../NavElement";
 
 function BottomNav() {
   return (
-    <div className="flex bg-primary-gray rounded-tl-2xl rounded-tr-2xl w-[75%] h-12 justify-center items-center gap-10 z-10 absolute bottom-0 left-[25%] -ml-11">
+    <div className="flex bg-primary-gray rounded-tl-2xl rounded-tr-2xl w-[75%] h-12 justify-center items-center gap-10 z-10 fixed bottom-0 left-[25%] -ml-11">
       <NavElement iconNumber={1} active={true} />
       <NavElement iconNumber={2} />
       <NavElement iconNumber={3} />
