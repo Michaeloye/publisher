676b417f92c660fdf26154321c7104757f8d9e09
michaeloye <michaeloye23@gmail.com>
2022-04-13 19:22:02 +0100

    Add mobile styling for button

--- src/components/StartTopic.jsx
+++ src/components/StartTopic.jsx
@@ -3,10 +3,15 @@ import { BsPlus } from "react-icons/bs";
 
 function StartTopic() {
   return (
-    <div className="bg-primary-blue w-40 h-10 md:w-60 md:px-10 rounded-lg text-white flex items-center justify-center">
+    <button
+      className="bg-primary-blue flex items-center justify-center w-12 h-12 md:w-60 md:px-10 rounded-full md:rounded-lg text-white 
+     fixed bottom-12 right-5 md:static md:bottom-auto md:right-auto"
+    >
       <BsPlus size={25} />
-      <p className="font-semibold text-sm md:text-base">Start New Topic</p>
-    </div>
+      <p className="hidden md:block font-semibold text-sm md:text-base">
+        Start New Topic
+      </p>
+    </button>
   );
 }
 
