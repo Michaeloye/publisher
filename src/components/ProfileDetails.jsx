ae2c860495439964536c57796f8cd83533e115c1
michaeloye <michaeloye23@gmail.com>
2022-04-14 20:14:09 +0100

    Add Logout handler

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import ProfilePicture from "./ProfilePicture";
 
 function ProfileDetails({ userName }) {
+  function handleLogout() {
+    localStorage.removeItem("name");
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    window.location.href = `/`;
+  }
   return (
-    <div className="flex items-center ml-10 md:ml-20 mt-10 gap-7">
-      <ProfilePicture size="large" />
-      <h1 className="text-lg md:text-2xl font-bold">{userName}</h1>
+    <div className="flex items-center justify-between ml-10 md:ml-20 mt-10">
+      <div className="flex items-center gap-3 md:gap-5">
+        <ProfilePicture size="large" />
+        {/* text-left because by default text is aligned to the center */}
+        <h1 className="text-lg text-left md:text-2xl font-bold w-[100px] md:w-[300px] overflow-x-hidden text-ellipsis">
+          {userName}
+        </h1>
+      </div>
+      <button
+        className="bg-red-700 text-base md:text-lg text-white px-5 py-2 mr-4 rounded-md shadow "
+        onClick={() => handleLogout()}
+      >
+        Logout
+      </button>
     </div>
   );
 }
