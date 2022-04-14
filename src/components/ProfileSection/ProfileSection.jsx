a2c71c59abe26c65b1676509fa111feedbe10d56
michaeloye <michaeloye23@gmail.com>
2022-04-14 20:20:11 +0100

    Add user Comments

--- src/components/ProfileSection/ProfileSection.jsx
+++ src/components/ProfileSection/ProfileSection.jsx
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from "react";
 import Comment from "../Comment/Comment";
 import ProfileDetails from "../ProfileDetails";
+import BasicLoader from "../Loader/BasicLoader";
+import axios from "axios";
 
 function ProfileSection() {
   const [userName, setUserName] = useState("");
+  const [userId, setUserId] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const name = localStorage.getItem("name");
+    const userId = localStorage.getItem("userId");
     setUserName(name);
+    setUserId(userId);
     return () => {
       null;
     };
+  }, []);
+
+  useEffect(() => {
+    // reversing the array so as to be sorted in ascending order from recent to long ago
+    axios
+      .get("https://mikepostapp.herokuapp.com/feed/posts")
+      .then((res) => {
+        setPosts(res.data.posts.reverse());
+        setLoading(false);
+      })
+      .catch((err) => console.log(err.message));
   }, []);
 
   return (
@@ -26,41 +44,24 @@ function ProfileSection() {
         <p className="text-left ml-20 md:ml-32 font-semibold mt-3 mb-3">
           Latest posts
         </p>
-        <Comment
-          title="lorem ispium"
-          description="
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-    "
-          author="andrew neil"
-        />
-        <Comment
-          title="lorem ispium"
-          description="
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-  "
-          author="andrew neil"
-        />
-        <Comment
-          title="lorem ispium"
-          description="
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-  "
-          author="andrew neil"
-        />
-        <Comment
-          title="lorem ispium"
-          description="
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-  "
-          author="andrew neil"
-        />
-        <Comment
-          title="lorem ispium"
-          description="
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero esse a eligendi, cum accusamus debitis consequatur fugit saepe veritatis atque? Exercitationem, esse tempora mollitia dignissimos minus illo omnis quidem?
-  "
-          author="andrew neil"
-        />
+        {loading && (
+          <div className="flex items-center justify-center">
+            <BasicLoader />
+          </div>
+        )}
+        {posts
+          .filter((post) => post.creator._id === userId)
+          .map((post) => {
+            return (
+              <Comment
+                key={post._id}
+                title={post.title}
+                content={post.content}
+                author={post.creator.name}
+                since={post.createAt}
+              />
+            );
+          })}
         <br />
         <br />
         <br />
