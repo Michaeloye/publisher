b6560821576781a78bd8c5a85973464a570a3e9f
michaeloye <michaeloye23@gmail.com>
2022-04-14 19:11:06 +0100

    Add "Coming Soon"

--- src/pages/Topics.jsx
+++ src/pages/Topics.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Topics() {
-  return <div>Topics</div>;
+  return (
+    <div className="flex flex-col mt-10 items-center justify-center text-2xl md:text-4xl font-semibold gap-5">
+      <h1>Coming Soon</h1>
+      <p>🚀</p>
+    </div>
+  );
 }
 
 export default Topics;
