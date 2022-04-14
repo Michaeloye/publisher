282d861ad436679a0aa2867789388957a933466d
michaeloye <michaeloye23@gmail.com>
2022-04-14 20:14:56 +0100

    Adjust width

--- src/components/LoginOrSignup.jsx
+++ src/components/LoginOrSignup.jsx
@@ -5,7 +5,7 @@ function LoginOrSignup({ text, onClick }) {
     // Depending on the text the button color is determined
     <div
       className={
-        `py-2 px-10 w-40 md:w-48 rounded-full font-medium cursor-pointer flex items-center justify-center ` +
+        `py-2 px-10 w-36 md:w-48 rounded-full font-medium cursor-pointer flex items-center justify-center ` +
         (text === "Login"
           ? `bg-transparent border border-primary-blue `
           : `bg-primary-blue border text-white `)
