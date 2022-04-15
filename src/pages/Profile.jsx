6256d1e678e4f87fb90167e502b29494ee6f2ac4
michaeloye <michaeloye23@gmail.com>
2022-04-15 18:20:32 +0100

    Add StartTopic modal

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,20 @@
 import { useState, useEffect } from "react";
 import BottomNav from "../components/BottomNav/BottomNav";
+import StartTopicModal from "../components/Modal/StartTopicModal";
 import OtherInfo from "../components/OtherInfo/OtherInfo";
 import ProfileSection from "../components/ProfileSection/ProfileSection";
 import SideNav from "../components/SideNav/SideNav";
+import StartTopic from "../components/StartTopic";
 import useScreensize from "../hooks/useScreensize";
 
 function Profile() {
   const [smallScreen, setSmallScreen] = useState("");
+  const [startTopicModalOpen, setStartTopicModalOpen] = useState(false);
   const { width } = useScreensize();
+
+  const handleStartTopicModal = (state) => {
+    setStartTopicModalOpen(state);
+  };
 
   useEffect(() => {
     if (width >= 640) {
@@ -19,11 +26,37 @@ function Profile() {
       null;
     };
   }, [width]);
+
+  // if user is not logged in redirect to landing page
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token === null) {
+      window.location.href = "/#";
+    } else {
+      // verify token here
+    }
+  }, []);
+
   return (
     <div className="md:flex md:justify-around">
       {smallScreen ? <BottomNav /> : <SideNav />}
       <ProfileSection />
-      {smallScreen ? "" : <OtherInfo />}
+      {startTopicModalOpen && (
+        <StartTopicModal handleClose={handleStartTopicModal} />
+      )}
+
+      {smallScreen ? (
+        <StartTopic onClick={() => handleStartTopicModal(true)} />
+      ) : (
+        ""
+      )}
+
+      {/* other */}
+      {smallScreen ? (
+        ""
+      ) : (
+        <OtherInfo onClick={() => handleStartTopicModal(true)} />
+      )}
     </div>
   );
 }
