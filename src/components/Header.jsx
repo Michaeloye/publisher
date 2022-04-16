b10dd3eb446f2dd50e979246e32968a4ed9a273f
michaeloye <michaeloye23@gmail.com>
2022-04-16 22:03:32 +0100

    Show Notification and ProfilePicture based on path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+
 import Logo from "./Logo";
 import Notification from "./Notification";
 import ProfilePicture from "./ProfilePicture";
 import SearchBar from "./SearchBar";
 
 function Header() {
+  // get url pathname
+  const location = useLocation().pathname;
+
   return (
     <div className="h-12 md:h-14 bg-white border flex items-center justify-between">
       <div className="ml-3 md:ml-10">
@@ -16,10 +20,15 @@ function Header() {
       {/* Notification and ProfilePicture are in a single div because from the design they are closer
       to each other  */}
       <div className="flex items-center gap-5">
-        <Notification />
-        <Link to={"/profile"} className="mr-3 md:mr-10">
-          <ProfilePicture />
-        </Link>
+        {/* if location is not the root page "welcome page" show the Notification and ProfilePicture */}
+        {location !== "/" && (
+          <>
+            <Notification />
+            <Link to={"/profile"} className="mr-3 md:mr-10">
+              <ProfilePicture />
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
