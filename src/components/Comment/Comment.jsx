b26fdbd458412c2f911878b83f092e41e5a894c7
michaeloye <michaeloye23@gmail.com>
2022-04-19 02:12:16 +0100

    Add edit and delete button

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import timeSince from "../../utils/timeSince";
+
 import LikeDislike from "../LikeDislike";
 import ProfilePicture from "../ProfilePicture";
+
+import { FiEdit3 } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+
 import { motion } from "framer-motion";
 
-function Comment({ title, content, author, since }) {
+function Comment({ authorId, title, content, author, since }) {
   // animation variant
   const comment = {
     hidden: { opacity: 0, y: -100 },
@@ -12,9 +18,20 @@ function Comment({ title, content, author, since }) {
     exit: { opacity: 0, y: "100vh" },
   };
 
+  const [isMyComment, setIsMyComment] = useState();
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    console.log("authorId", authorId);
+    console.log("userId", userId);
+
+    // check if authorId and userId match
+    setIsMyComment(authorId === userId);
+  }, []);
+
   return (
     <motion.div
-      className="bg-white h-auto w-[90%] md:w-[80%] mx-auto rounded-md flex text-left pt-7 pr-4 md:pr-5 pb-5 shadow-lg mb-5"
+      className="bg-white relative h-auto w-[90%] md:w-[80%] mx-auto rounded-md flex text-left pt-7 pr-4 md:pr-5 pb-5 shadow-lg mb-5"
       key="comment"
       initial="hidden"
       animate="visible"
@@ -39,9 +56,26 @@ function Comment({ title, content, author, since }) {
           use the new Date func to convert to proper date string in which it can be used in 
           timeSince func */}
           <p className="text-gray-600 whitespace-nowrap">
-            {timeSince(new Date(`${since}`))} ago
+            {timeSince(new Date(`${since}`))}
           </p>
         </div>
+        {/* Edit and delete button shown based on if the comment belongs to the user => authorId and userId match*/}
+        {isMyComment && (
+          <div className="flex gap-3 absolute top-2 right-3 ">
+            <button
+              className="bg-primary-blue border border-primary-blue rounded px-3 py-1 hover:bg-transparent 
+          text-white hover:text-primary-blue"
+            >
+              <FiEdit3 size={18} className="" />
+            </button>
+            <button
+              className="bg-red-700 border border-red-700 rounded px-3 py-1 hover:bg-transparent 
+          text-white hover:text-red-700"
+            >
+              <MdDelete size={18} />
+            </button>
+          </div>
+        )}
       </main>
     </motion.div>
   );
