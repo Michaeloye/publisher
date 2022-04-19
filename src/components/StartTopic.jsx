c82ab691a557b3095a26f98bbad58f8580dc6482
michaeloye <michaeloye23@gmail.com>
2022-04-19 22:48:15 +0100

    Add animation

--- src/components/StartTopic.jsx
+++ src/components/StartTopic.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import { BsPlus } from "react-icons/bs";
 
+import { motion } from "framer-motion";
+
 function StartTopic({ onClick }) {
+  const startTopicButton = {
+    whileHover: { scale: 0.98 },
+    whileTap: { scale: 0.98 },
+  };
+
   return (
-    <button
+    <motion.button
       className="bg-primary-blue flex items-center justify-center w-12 h-12 md:w-60 md:px-10 rounded-full md:rounded-lg text-white 
-     fixed bottom-12 right-5 md:static md:bottom-auto md:right-auto"
+     fixed bottom-12 right-5 md:static md:bottom-auto md:right-auto hover:bg-opacity-80"
       onClick={() => onClick()}
+      key="start-topic-button"
+      whileHover="whileHover"
+      whileTap="whileTap"
+      variants={startTopicButton}
     >
       <BsPlus size={25} />
       <p className="hidden md:block font-semibold text-sm md:text-base">
         Start New Topic
       </p>
-    </button>
+    </motion.button>
   );
 }
 
