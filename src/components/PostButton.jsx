458d27b61990dc25735a122bc4d267918cd3fc7f
michaeloye <michaeloye23@gmail.com>
2022-04-20 15:04:24 +0100

    Add placeholder prop

--- src/components/PostButton.jsx
+++ src/components/PostButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { motion } from "framer-motion";
 
-function PostButton() {
+function PostButton({ placeholder = "Post" }) {
   const postButton = {
     whileHover: { scale: 0.98 },
     whileTap: { scale: 0.98 },
@@ -11,13 +11,13 @@ function PostButton() {
   return (
     <motion.button
       type="submit"
-      className="bg-primary-blue rounded-full h-12 w-40 flex justify-center items-center text-white font-semibold bg-opacity-80"
+      className="bg-primary-blue rounded-full h-12 w-40 flex justify-center items-center text-white font-semibold hover:bg-opacity-80"
       key="post-button"
       whileHover="whileHover"
       whileTap="whileTap"
       variants={postButton}
     >
-      Post
+      {placeholder}
     </motion.button>
   );
 }
